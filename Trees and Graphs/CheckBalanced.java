c2817ba81d0701626e28a395edd5bb3a0eff7bc6
Santosh Gunashekar <santoshguna001@gmail.com>
2019-07-07 17:13:12 +0530

    Check if a Binary Tree is BST

--- Trees and Graphs/CheckBalanced.java
+++ Trees and Graphs/CheckBalanced.java
@@ -1,7 +1,8 @@
 /**
- * Implement a function to check if a binary tree is balanced. For the purposes
- * of this question, a balanced tree is defied to be a tree such that the
- * heights of the two subtrees of any node never differ by more than one.
+ * 1. Implement a function to check if a binary tree is balanced. For the
+ * purposes of this question, a balanced tree is defied to be a tree such that
+ * the heights of the two subtrees of any node never differ by more than one..
+ * 2. Implement a function to check if a binary tree is a binary search tree.
  */
 class CheckBalanced {
     static class Node {
@@ -24,7 +25,49 @@ class CheckBalanced {
         return Math.max(leftHeight, rightHeight);
     }
 
+    public static boolean isBST(Node node) {
+        if (node == null) {
+            return true;
+        }
+        if (node.right != null && node.left != null) {
+            if (node.data >= node.left.data && node.data < node.right.data) {
+                return isBST(node.left) && isBST(node.right);
+            }
+            return false;
+        }
+        if (node.right != null && node.left == null) {
+            if (node.data < node.right.data) {
+                return isBST(node.right);
+            }
+            return false;
+        }
+        if (node.right == null && node.left != null) {
+            if (node.data >= node.left.data) {
+                return isBST(node.left);
+            }
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isBSTree(Node node) {
+        if (node == null) {
+            return true;
+        }
+        try {
+            if (node.data >= node.left.data && node.data < node.right.data) {
+                return isBSTree(node.left) && isBSTree(node.right);
+            }
+        } catch (NullPointerException ne) {
+            // ne.printStackTrace();
+        }
+        return false;
+    }
+
     public static boolean isBalanced(Node node) {
+        if (node == null) {
+            return true;
+        }
         int difference = getHeight(node.left, 0) - getHeight(node.right, 0);
         if (difference <= 1 && difference >= -1) {
             return true;
@@ -34,6 +77,9 @@ class CheckBalanced {
 
     public static void main(String[] args) {
 
+        Node head = new Node(6, null, null);
+        head.right = new Node(8, null, null);
+        head.left = new Node(3, null, null);
         /*
          * Node head = new Node(10, null, null); head.left = new Node(20, null, null);
          * head.right = new Node(30, null, null); head.left.left = new Node(40, null,
@@ -42,6 +88,7 @@ class CheckBalanced {
          * head.left.right.left = new Node(80, null, null); head.right.right.left = new
          * Node(90, null, null); head.right.right.left.left = new Node(100, null, null);
          */
-        System.out.println(isBalanced(head));
+        System.out.println(isBST(head));
+
     }
 }
